df3b1a319a4d2342d6a4d9ade0116b8f1b1f6cbf
Maicon <raticida@gmail.com>
2018-08-09 13:44:09 -0300

    BD config

--- includes/config.php
+++ includes/config.php
@@ -6,10 +6,10 @@ session_start();
 date_default_timezone_set('America/Sao_Paulo');
 
 //database credentials
-define('DBHOST','localhost');
-define('DBUSER','root');
-define('DBPASS','');
-define('DBNAME','borradoano');
+define('DBHOST','edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	');
+define('DBUSER','sm38r4alhb5fmgwj');
+define('DBPASS','n67igr2zbi9rh9c0');
+define('DBNAME','xgoosp5ef7msdibz');
 
 //application address
 define('DIR','http://localhost/');
